9c273230ec69ff643caa7574a953b8d49e88d499
Iskandar1412 <silvapaco810@gmail.com>
2024-08-11 20:08:24 -0600

    [ADD]: Se agregaron inserts

---
+++ Practicas/Practica1/DB/Procedures/INSERTS/inserts.sql
@@ -0,0 +1,20 @@
+-- Registro Usuario
+CALL PR1('John', 'Doe', 'john.doe@example.com', '1990-01-01', 'mypassword', 10);
+
+
+-- Creacion Curso
+CALL PR5('Fisica Basica', 11);
+CALL PR5('Matematica Basica 1', 0);
+
+-- Rol Estudiante/Tutor
+CALL PR4('Tutor');
+CALL PR4('Estudiante');
+
+-- Asignacion Curso
+CALL PR3('john.doe@example.com', 1);
+
+-- Cambio rol
+CALL PR2('john.doe@example.com', 1);
+
+-- Validacion Datos
+CALL PR6();
